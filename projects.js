ded176efb2bb581badbe1f00efdb34a3b98411a5
claralny <137039604+claralny@users.noreply.github.com>
2026-07-27 02:54:52 +0200

    mainn

--- projects.js
+++ projects.js
@@ -15,13 +15,20 @@ softwares : Blender | After Effects | Procreate | Illustrator | Photoshop
 
 
     `,
+    textFr: `
+début timide de visuel pour le son rokit de wasting shit prcq c trop dommage que y'ai pas de clip g expérimenté la 3D la typo globalement:ppppp ceci dit jsp si ce clip se finira<br><br><br>
+
+softwares : Blender | After Effects | Procreate | Illustrator | Photoshop
+`,
     video: "import/rokit.mp4",
     images: [
+
+      "import/rokitcityy.png",
       "import/rokitblend.png",
       "import/rokit2.mp4",
-      "import/rokitmetroo.png",
-      "import/rokitt.png",
-      "import/rokitcityy.png" 
+      "import/rokitmetroo.png"
+      
+      
       
     ],
 
@@ -41,6 +48,11 @@ mediums : Video | Engraving | Website | Drawing | Installation
 
 
     `,
+    textFr: `
+petite expo sur le thème des barrières linguistiques réalisée avec un copain allemand en croatie ct très fun on a pris ce qu'on avait sous la main (miroir, projecteur, encre, pilules, perruque……………….) et ça rend bien<br><br><br>
+
+mediums : Video | Engraving | Website | Drawing | Installation
+`,
     video: "import/suw.mp4",
     images: [
       "import/suw1.jpeg",
@@ -68,14 +80,19 @@ mediums : Video | Engraving | Website | Drawing | Installation
     title: "♬⋆.˚vjing",
     year: "2026",
     text: `
-      visuals created for the VJing of a concert organized by Krushh and 10form, featuring performances by Odden, Luso, Baton, MP404, and Err Walou.<br><br>
+      visuals created for the vjing of a concert organized by krushh and 10form, featuring performances by odden, luso, baton, mp404, and err walou.<br><br>
 
-I produced both modular and fixed visuals for each artist. during the event, a collaborator and I controlled the TouchDesigner setup in real time using a MIDI controller, adapting the visuals to each performance.<br><br><br>
+i produced both modular and fixed visuals for each artist. during the event, a collaborator and I controlled the TouchDesigner setup in real time using a MIDI controller, adapting the visuals to each performance.<br><br><br>
 
 softwares : TouchDesigner | After Effects | Photoshop
 
 
     `,
+    textFr: `
+plein de vidéos pour un concert organisé par krushh et 10form avec plein d'artistes (err walou mp404 odden luso baton…………) réalisé en coprod mais là y'a que les visuels que j'ai fait ;pp<br><br><br>
+
+softwares : TouchDesigner | After Effects | Photoshop
+`,
     video: "import/vjing.mp4",
     images: [
       "import/vjingerrwalou.mp4",
@@ -93,10 +110,15 @@ softwares : TouchDesigner | After Effects | Photoshop
     title: "bad!!!!!!⚠⚠⚠⚠⚠boy!!!!!!⚠⚠⚠⚠⚠",
     year: "2026",
     text: `
-      frame-by-frame animation based on a voice message recounting the theft of a Gucci cap.<br><br><br>
+      frame-by-frame animation based on a voice message recounting the theft of a gucci cap.<br><br><br>
 softwares : Procreate
 
     `,
+    textFr: `
+un jour on m'a envoyé un vocal insta sur le vol d'une casquette gucci je me devais d'en faire une animation c tout<br><br><br>
+
+softwares : Procreate
+`,
     video: "import/badboy.mp4",
     images: [
       // "import/gp.png",
@@ -125,6 +147,11 @@ i composed a ten-minute soundtrack in Strudel while developing an audio-reactive
 softwares : Strudel | Visual Studio Code (html/css/p5.js) | Playtronica
 
     `,
+    textFr: `
+projet basé sur du live coding [bip bip bip bip] sur le thème d fleurs voila fallait coder une musique de 10 minutes sur ce thème et faire d visus avec ct trop mimi en + quand on touche les fleurs ça change le visuel #nerd!!!!!!!<br><br><br>
+
+softwares : Strudel | Visual Studio Code (html/css/p5.js) | Playtronica
+`,
     video: "import/octetgarden.mp4",
     images: [
       "import/octetgarden1.png",
@@ -148,6 +175,11 @@ ghettoprincess: {
       animation on the theme of the “ghetto princess”, oscillating between her love for romance, lace, and reflections on her appearance, and the harsher aspects of her life, including verbal and physical violence. <br><br>it balances drama and celebration, with misadventures hidden beneath romance and appearance. the animation is accompanied by a poetic text blending fairy tale codes with a more raw and contemporary language.<br><br><br>
       softwares : After Effects | Procreate
     `,
+    textFr: `
+animation d'une ghetto princess qui en a trop marre que le mascara lui aille pas et qui en a encore plus marre de kiffer des gros nazes après elle a des épées et elle adore adidas<br><br><br>
+
+softwares : After Effects | Procreate
+`,
     video: "import/ghettoprincessstoryclara.mp4",
     images: [
       // "import/gp.png",
@@ -180,6 +212,11 @@ ghettoprincess: {
       <br><br><br>
       softwares & other : InDesign | Tattoo printer | Engraving pen
     `,
+    textFr: `
+d fois je m'amuse à graver d trucs quand j'ai personne a tatouer [talons en cuirrrrrr, échiquier en verre, verre, dague, dague 2, super superstar, peau, miroir mon beau miroir, casque de moto] j'ai gravé le même motif sur tous mes supports pour voir s'il restait le même selon ce sur quoi je grave et la réponse est grave pas mdr mais c joli quand même<br><br><br>
+
+softwares & other : InDesign | Tattoo printer | Engraving pen
+`,
     video: "import/gravureedition.mp4",
     images: [
       "import/couverture.png",
@@ -208,6 +245,11 @@ alongside the video, I developed fictional poster experiments for an Angel Candl
 
 softwares : After Effects | Touch Designer | Photoshop
 `,
+textFr: `
+mini mini clip musical pour le son angel candles de notinbed et clara kimera tout est globalement basé sur les paroles y'a de la vidéo du montage et tout en plus d'une affiche fictive pour une possible release party…………..<br><br><br>
+
+softwares : After Effects | Touch Designer | Photoshop
+`,
 video: "import/notinb.mp4",
 images: [
 "import/angelcandleswebsite.png",
@@ -233,6 +275,12 @@ to emulate a VJ workflow using the numpad, manual, real-time control without tra
 software : Visual Studio Code (html/css/p5.js)<br><br>
 <a href="https://ghettoprincesss.github.io/worldisastercompilation/" target="_blank">[see project]</a>
 `,
+textFr: `
+un matin je me suis réveillée chez une copine et g écouté ce son pour la première fois avant d'aller en cours tellement je l'ai écouté je suis arrivée en retard et ça tombe bien fallait une musique pour un projet de [bip bip] code [boup boup] alors voilà c un projet de site web interactif qui imite le taff d'un vj mais sans matos de vj [touch designer contrôleur…………] ici c que le numpad qui crée le chaos la compilation de désastres mondiaux<br><br><br>
+
+software : Visual Studio Code (html/css/p5.js)<br><br>
+<a href="https://ghettoprincesss.github.io/worldisastercompilation/" target="_blank">[see project]</a>
+`,
 video: "import/w.mp4",
 images: [
 "import/w.png",
@@ -254,6 +302,13 @@ our approach highlighted architectural details and symmetrical forms. I was resp
 softwares :   After Effects | Resolume Arena<br>
 collaboration :  Serena Taleb | Nathan Sonzogni<br>
 music : the dreamer in her - Thomas Azier`,
+textFr: `
+video mapping sur le thème d cathédrales [c vraiment trop beau] projeté sur 3 malheureuses colonnes faites de carton mais ça en jette grave, on est 3 derrière ce projet<br><br><br>
+
+softwares :   After Effects | Resolume Arena<br>
+collaboration :  Serena Taleb | Nathan Sonzogni<br>
+music : the dreamer in her - Thomas Azier
+`,
 video: "import/chamb.mp4",
 images: [
 "import/chamb.png",
@@ -271,6 +326,11 @@ experimental music video built around amne’s sonic world, mixing audio-driven 
 the visuals evolve with the music, creating a shifting exploration of identity where sound and image merge to express the track’s energy.<br><br><br>
 
 softwares : After Effect | Touch Designer | Photoshop <br>`,
+textFr: `
+j'avais plus rien pour mon portfolio en vrai et j'aime bien ce son d'amne ya du montage photo [PS PS PS PS] d trucs audio réactifs…….[encore]……..de la vidéo etc etc<br><br><br>
+
+softwares : After Effect | Touch Designer | Photoshop <br>
+`,
 video: "import/amneberll.mp4",
 images: [
 "import/berlin1.png",
@@ -293,6 +353,13 @@ presenting it in VR immerses the audience
 in a direct and intense audiovisual experience.<br><br><br>
 
 softwares : Blender | After Effects<br><br>
+
+<a href="https://www.youtube.com/watch?v=lffc7b8xnLo" target="_blank">[see project]</a>
+`,
+textFr: `
+projet que je pensais jamais win c un environnement vr créé dans blender qui interprète datamatics de ryoki ikeda et qui explore la relation entre données son et images [ryoki ikeda utilise d données brutes pour générer des visuels et des sons en même temps ça fait réfléchir aux données qui nous entourent et comment elles pourraient être autres]<br><br><br>
+
+softwares : Blender | After Effects<br><br>
 
 <a href="https://www.youtube.com/watch?v=lffc7b8xnLo" target="_blank">[see project]</a>
 `,
@@ -321,6 +388,11 @@ the work combines generative art, editing, and 3D techniques.<br><br><br>
 softwares : TouchDesigner | After Effects
 
 `,
+textFr: `
+je voulais trop essayer touch designer alors j'ai fait un clip pour lili castiglioni prcq elle est trop forte ya de l'audio réact des montages photos et aussi vidéo c très coquette et mystérieux…………….<br><br><br>
+
+softwares : TouchDesigner | After Effects
+`,
 video: "import/griefseedclipvideoclara.mp4",
 images: [
 "import/gs1.png",
@@ -345,6 +417,12 @@ a short story<br><br><br>
 softwares: InDesign | Touch Designer | Artivive | Photoshop<br><br>
 <a href="https://claralny.github.io/spray/" target="_blank">[see project]</a>
 `,
+textFr: `
+ptn de projet d'école avec comme sujet unique le spray moi je suis trop triste je pshhhhttt et pssshhhtttt des trucs en vrac et après j'essaye de leur donner du sens avec d phrases trop tristes en plus c audio réactif et ya de la réalité augmentée et un fanzine et un site web bref……….un mot vaut mille supports quoi<br><br><br>
+
+softwares: InDesign | Touch Designer | Artivive | Photoshop<br><br>
+<a href="https://claralny.github.io/spray/" target="_blank">[see project]</a>
+`,
 video : "import/claraspray_projection.mp4",
 images: [
 "import/clara_17.png",
@@ -364,6 +442,10 @@ through my thesis, I examine the connection between the electronic music scene a
 i study the role of visuals in electronic music, particularly through audiovisual performances, which oscillate between dialogue and transcription.<br><br>
 
 how does the embodiment of electronic music through graphic design influence the audience’s experience?
+`,
+textFr: `
+mémoire que j’ai écrit sur l’étude du lien entre la scène de la musique électro et le graphisme prcq en vrai dans la scène électro y’a plein de performances audiovisuelles qui oscillent entre dialogue et transcription…..nuance
+en gros je me demande comment l'incarnation de la musique électronique par le graphisme influence-t-elle l'expérience du public ?
 `,
 video: null,
 images: [
@@ -388,6 +470,10 @@ the project follows a poetic text, moving
 from tenderness to emotional collapse.<br><br>
 using only tracing paper gives an editorial form to my fragile vision 
 of love, evoking the confusion and blending that this love can create.`,
+textFr: `
+« parce que parfois, les mots se brisent avant d'atteindre la douleur du cœur, et parce que certains jours, on continue de respirer sans savoir pourquoi. parce que les chagrins modernes ne se racontent plus par l'écriture, et parce que les blessures de l'âme méritent d'être vues. »<br><br>
+g écrit ça en chouinant parce que gt trop trop triste (;´༎ຶٹ༎ຶ\`) c un petit livre qui suit ma vision du romantisme il est fait uniquement de calque prcq c très beau et prcq ça illustre bien la vision fragile de l'amour [papier très fragile que j'ai déchiré malencontreusement très souvent……..] ça montre aussi la confusion le mélange que l'amour peut créer en plus jsuis médisante je dis toujours que je sais comment ça va finir ça tombe bien prcq avec le calque on connaît tjrs la suite vu que c du papier transparent
+`,
 video: "import/ultraultracool.mp4",
 images: ["import/ultrav.png"]
 },
@@ -429,6 +515,11 @@ of glitch.The edit explores the human silhouette and clothing, gradually focusin
 the final pages visually convey an emotional journey : from sadness to fear, through anger, and ultimately to freedom.<br><br><br>
 softwares : Processing 4 | InDesign
 `,
+textFr: `
+projet qui combine code et édition centré sur l’expérimentation du pixel sorting (c quand les pixels d’une image sont réarrangés etc) toute l’édition explore crescendo le vêtement puis la silhouette humaine le visage et enfin les émotions………………a la fin y’a une mini histoire avec les résultats les plus parlants genre t triste après t vide etc mais ça finit bien quand même<br><br><br>
+
+softwares : Processing 4 | InDesign
+`,
 video: null,
 images: [
     "import/de4.png",
@@ -454,6 +545,12 @@ created and displayed as part of a workshop organized by graphic designer Robin 
 
 print : risography<br>
 artist : Lana del Rey`,
+textFr: `
+expérimentations de mise en page typographique sur des affiches pour la 2e édition du festival de musique fictif Madfest. créées et exposées dans le cadre d'un atelier organisé par le graphiste Robin Abreu (mrc ct la frappe)<br><br><br>
+
+print : risography<br>
+artist : Lana del Rey
+`,
 video: null,
 images: [
 "import/mad0.png",
